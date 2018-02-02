619d6845176054c1cbfa649ab51ff5ab7d048b5b
Mateusz Cieplak <mateusz.cieplak@gft.com>
2018-02-02 08:52:37 +0100

    Add method getBooksAlphabeticallyByTitle to Book service

--- src/main/java/com/mmc/db/DbAppDemo/services/BookService.java
+++ src/main/java/com/mmc/db/DbAppDemo/services/BookService.java
@@ -1,7 +1,38 @@
 package com.mmc.db.DbAppDemo.services;
 
+import com.mmc.db.DbAppDemo.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BookService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Book> getBooksAlphabeticallyByTitle(){
+        String query = "SELECT * FROM ksiazki ORDER BY tytul ASC";
+
+        List<Book> books = jdbcTemplate.query(query, new RowMapper<Book>() {
+            @Override
+            public Book mapRow(ResultSet resultSet, int i) throws SQLException {
+                Book book = new Book();
+                book.setId(resultSet.getLong("idksiazki"));
+                book.setAuthorsName(resultSet.getString("imieautora"));
+                book.setAuthorsSurname(resultSet.getString("nazwiskoautora"));
+                book.setTitle(resultSet.getString("tytul"));
+                book.setPrice(resultSet.getShort("cena"));
+
+                return book;
+            }
+        });
+
+        return books;
+    }
 }
--- src/test/java/com/mmc/db/DbAppDemo/BookServiceTests.java
+++ src/test/java/com/mmc/db/DbAppDemo/BookServiceTests.java
@@ -1,20 +1,31 @@
 package com.mmc.db.DbAppDemo;
 
+import com.mmc.db.DbAppDemo.model.Book;
+import com.mmc.db.DbAppDemo.services.BookService;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("files:/src/main/resources/jdbc-config.xml")
+
+@ContextConfiguration("file:src/main/resources/jdbc-config.xml")
 public class BookServiceTests {
+
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    BookService bookService;
 
     @Test
-    public void should(){
+    public void shouldReturnListOfBooksOrderedByNameAsc(){
+        List<Book> booksOrderefByAc = bookService.getBooksAlphabeticallyByTitle();
+
+        Assertions.assertThat(booksOrderefByAc).isNotEmpty();
+        Assertions.assertThat(booksOrderefByAc.get(0).getTitle()).isEqualTo("HTML5. Tworzenie witryn");
+        Assertions.assertThat(booksOrderefByAc.get(booksOrderefByAc.size()-1).getTitle()).isEqualTo("Zaawansowane programowanie w PHP");
 
     }
 }
